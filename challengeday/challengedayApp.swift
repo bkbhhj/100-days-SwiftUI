1423ca59c32ce0b8646bd5d0f21a9212cec8a30e
Pavel Timofeev <ttt.1.12@bk.ru>
2022-05-20 00:18:05 +0700

    Initial Commit

---
+++ challengeday/challengedayApp.swift
@@ -0,0 +1,17 @@
+//
+//  challengedayApp.swift
+//  challengeday
+//
+//  Created by Павел Тимофеев on 20.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct challengedayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
